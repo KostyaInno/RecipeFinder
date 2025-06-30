3c26ca6e3b0b96ce1bbdb5a22aeb9a8cad57ec0a
Kostya Tsyvilko <kanstantsin.tsyvilko@innowise.com>
2025-06-30 13:49:07 +0200

    Initial Commit

---
+++ AIGeneratedApp/AIGeneratedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIGeneratedAppApp.swift
+//  AIGeneratedApp
+//
+//  Created by kostya on 30/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIGeneratedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
